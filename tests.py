833839fc0df154a024d4f33a238e1a062be4883e
Svetlana Skirdova <tyurinaswetlanochcka@yandex.ru>
2026-07-14 19:35:06 +0700

    add test_get_books_with_specific_genre_get_two_books

--- tests.py
+++ tests.py
@@ -47,3 +47,13 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
+
+    def test_get_books_with_specific_genre_get_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Ужасы')
+        assert len(collector.get_books_with_specific_genre('Ужасы')) == 2
+
+    
